a61d083c049cd5260fd9fc9e6ee2e4abb1705610
Taylor Shuff <tkylesh@gmail.com>
2016-08-02 12:20:03 -0500

    added some more details to js file.

--- for-loop.js
+++ for-loop.js
@@ -1,1 +1,22 @@
 /*JS file for loops*/
+
+
+
+
+//4. Object iteration
+
+/*here's the object:
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}*/
