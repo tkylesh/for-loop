250cc2066f82d6db1e480edb00e22e0b820410c8
Taylor Shuff <tkylesh@gmail.com>
2016-08-02 13:17:43 -0500

    wrote out algorithm

--- for-loop.js
+++ for-loop.js
@@ -1,9 +1,35 @@
 /*JS file for loops*/
 
+//1. increment by 10
+  //for loop
+  //initialize = 5
+  //increment by 10
+  //console.log(i)
+  //stop if i > 120
+
+
+//2.  Decrement by Division
+  //for loop 
+  //intialize at 4096
+  //each loop divide i/2
+  //console.log(i)
+  //stop when i === 1
+
+
+//3. Array Iterations
+  //create array
+  //contains name of Am. Presidents
+  //loop over w/ for loop
+  //use string concatenation to console.log() to output order and name of each president
+  //ex: President #1 was George Washington
+        //President #2 was John Adams
+
 
 
 
 //4. Object iteration
+  //Use key value for-loop to iterate over object
+  //console.log() name of keys in the object
 
 /*here's the object:
 
